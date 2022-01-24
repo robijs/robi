091233251b60dd7adedf9e54b52c66c9dacd4c81
Stephen Matheis <stephen.a.matheis@gmail.com>
2022-01-24 09:39:07 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // This file can be edited programmatically.
 // If you know the API, feel free to make changes by hand.
-// Just be sure to put @START and @END sigils in the right places.
-// Otherwise, changes made with GUI tools will not render properly.
+// Just be sure to put @START, @END, and @[Spacer Name] sigils in the right places.
+// Otherwise, changes made from CLI and GUI tools won't work properly.
 
 import { Start } from './Robi/Robi.js'
 
