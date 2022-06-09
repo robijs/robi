2a67fb850e37a7a04d2ebdf14b4c7dcc2cca30cf
Stephen Matheis <stephen.a.matheis@gmail.com>
2022-06-09 18:52:35 -0500

    Update cleanup.js

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -3,10 +3,14 @@ import chalk from 'chalk'
 
 let dirs = await readdir('./src/Routes');
 
-dirs = dirs.filter(dir => dir.includes('_ARCHIVED'))
+dirs = dirs.filter(dir => dir.includes('_ARCHIVED'));
+
+console.log(`(${dirs.length}) routes to delete\n`);
 
 for (let dir of dirs) {
     console.log(`${chalk.red('⨂')} Removed archived route at ./src/Routes/${dir}`);
 
     await rm(`./src/Routes/${dir}`, { recursive: true, force: true });
 }
+
+console.log(chalk.green(`\n(${dirs.length}) routes deleted\n`));
