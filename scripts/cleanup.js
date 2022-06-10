179c727db94fc9eac17d312074323fa81c150610
Stephen Matheis <stephen.a.matheis@gmail.com>
2022-06-09 19:11:41 -0500

    Update cleanup.js

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -5,6 +5,11 @@ let dirs = await readdir('./src/Routes');
 
 dirs = dirs.filter(dir => dir.includes('_ARCHIVED'));
 
+if (dirs.length === 0) {
+    console.log(chalk.green(`/src/Routes is clean. There are no archived routes to delete.\n`));
+    process.exit();
+}
+
 console.log(`(${dirs.length}) routes to delete\n`);
 
 for (let dir of dirs) {
